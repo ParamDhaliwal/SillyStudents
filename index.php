b902b1ddfc263eea31e7077117d7bf7a1b4fff63
TacklessNebula3 <veerdhaliwal22@gmail.com>
2019-01-13 15:39:29 -0800

    Student class: 	- Formatted output little more. 	- Added myself as a student. Signed-off-by:TacklessNebula3 <veerdhaliwal22@gmail.com>

--- index.php
+++ index.php
@@ -11,34 +11,45 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include('Student.php');
-        
-        $students = array();
-        
-        foreach ($students as $student) {
-            echo $student->toString();
-        }
-        
-        $first = new Student();
-        $first->surname = "Doe";
-        $first->first_name = "John";
-        $first->add_email('home', 'john@doe.com');
-        $first->add_email('work', 'jdoe@mcdonalds.com');
-        $first->add_grade(65);
-        $first->add_grade(75);
-        $first->add_grade(55);
-        $students['j123'] = $first;
-        
-        $second = new Student();
-        $second->surname = "Einstein";
-        $second->first_name = "Albert";
-        $second->add_email('home','albert@braniacs.com');
-        $second->add_email('work1','a_einstein@bcit.ca');
-        $second->add_email('work2','albert@physics.mit.edu');
-        $second->add_grade(95);
-        $second->add_grade(80);
-        $second->add_grade(50);
-        $students['a456'] = $second;
+            include('Student.php');
+
+            $students = array();
+
+            $first = new Student();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('Home email', 'john@doe.com');
+            $first->add_email('Work email', 'jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('Home email','albert@braniacs.com');
+            $second->add_email('Work email 1','a_einstein@bcit.ca');
+            $second->add_email('Work email 2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            
+            $third = new Student();
+            $third->surname = "Daliwal";
+            $third->first_name = "Parm";
+            $third->add_email('Work email','p_dhalwial@my.bcit.ca');
+            $third->add_grade(88);
+            $third->add_grade(82);
+            $second->add_grade(96);
+            $students['p789'] = $third;
+
+            ksort($students);
+            
+            foreach ($students as $student) {
+                echo $student->toString();
+            }
         ?>
     </body>
 </html>

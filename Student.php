0d53225c75a646a6843f6081a60b26c38109a640
TacklessNebula3 <veerdhaliwal22@gmail.com>
2019-01-13 13:12:51 -0800

    Student class - Made little changes. Added functions like: average(), to get average grades. toString(), to format the text representation. Signed-off-by:TacklessNebula3 <veerdhaliwal22@gmail.com>

--- Student.php
+++ Student.php
@@ -26,8 +26,28 @@ class Student {
         $this->emails[$which] = $address;
     }
     
-    //Setting grades addresses
+    //Setting grades
     function add_grade($grade) {
-        $this->grades[] = grade;
+        $this->grades[] = $grade;
+    }
+    
+    //Calculating grades average
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value) {
+            $total += $value;
+        }
+        return $total / count($this->grades);
+    }
+    
+    //Formatting text
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' (' . $this->average() . ")\n";
+        foreach ($this->emails as $which => $what) {
+            $result .= $which . ': ' . $what . "\n";
+        }
+        $result .= "\n";
+        return '<pre>' . $result . '</pre>';
     }
 }
